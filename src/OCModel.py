a1d7f4cde4cd5fcba646122b86dc539c829f5f45
kanehekili <kanehekili@msi>
2024-03-19 02:23:01 +0100

    V0.9 Model & UI functional

--- src/OCModel.py
+++ src/OCModel.py
@@ -4,8 +4,12 @@ Created on 18 Jan 2024
 @author: matze
 '''
 #install python-openpyxl (arch native) or pylightxl which handles Softmaker better
+# pip install xlrd if xls files are necessary - else let them convert.. 
 from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.styles import Font
 from datetime import datetime
+from datetime import timedelta
 from time import sleep
 from enum import Enum
 from collections import Counter
@@ -48,8 +52,17 @@ class OSTools():
             res = os.path.join(res, tail)
         return res
     
-    def getHomeDirectory(self):
+    @classmethod
+    def getHomeDirectory(cls):
         return os.path.expanduser("~")
+    
+    @classmethod
+    def fileNameOnly(cls, path):
+        return os.path.basename(path)
+    
+    @classmethod
+    def dirname(cls,path):
+        return os.path.dirname(path)
     
     @classmethod
     def ensureDirectory(cls, path, tail=None):
@@ -141,33 +154,255 @@ class WorksheetReader():
     def __init__(self,filePath):
         self.file=filePath
     
-    def importXls(self):
+    def importXls(self,adaptercls):
+        filterFields = adaptercls.FIELDS
         wb = load_workbook(filename=self.file, read_only=True)
         sheets= wb.sheetnames
         ws=wb[sheets[0]]
         fullSheet=[]
         for row in ws.rows:
             singleRow=[]
+            col=0
+            for cell in row:
+                if col in filterFields:
+                    saveVal=cell.value if cell.value else ''
+                    singleRow.append(saveVal)
+                col +=1     
+            obj=adaptercls()
+            obj.row=singleRow
+            if not obj.isBlocked():
+                fullSheet.append(obj)        
+            '''
             for cell in row:
                 saveVal=cell.value if cell.value else '' 
                 singleRow.append(saveVal)
             fullSheet.append(singleRow)
+            '''
         cntMbrs=len(fullSheet)
-        cntCols = len(fullSheet[0])
+        head=fullSheet.pop(0)
+        cntCols = len(head.FIELDS)
         Log.info("Read %d entries, with %d columns each",cntMbrs,cntCols)
         wb.close()        
         return fullSheet   
 
-class WorksheetWriter():
+
+class OmocAdapter():
+    FIELDS = (1,2,3,5,6) #A=0
+    ID=5
+    BLOCKED=("Schulsport","Post-SV","gesperrt", "SVL")
+    def __init(self,tupleRow):
+        self.row=tupleRow()
+        
+    def fromDate(self):
+        raw=self._cleanDate() #has space in front..
+        hm = self.row[1]
+        return datetime.strptime(hm+raw,"%H:%M %d.%m.%Y")
+
+    def fromRoundDate(self):
+        raw=self.fromDate()
+        #lkr sometimes has 35. Round that to 30
+        return raw - timedelta(minutes=raw.minute %10)
+        
+    def toDate(self):    
+        raw=self._cleanDate()
+        hm = self.row[2]
+        return datetime.strptime(hm+raw,"%H:%M %d.%m.%Y")        
+    
+    def toRoundDate(self):
+        raw=self.toDate()
+        #lkr sometimes has 35. Round that to 30
+        return raw - timedelta(minutes=raw.minute %10)     
+    
+    def timeDelta(self):
+        return self.toRoundDate()-self.fromRoundDate()
+    
+    def _cleanDate(self):
+        return self.row[0].split(',')[1]
+    
+    def isBlocked(self):
+        key = self.row[3]
+        for item in self.BLOCKED:
+            if item in key:
+                #print("Blocked:",key)
+                return True
+        return False
+        
+    def location(self):
+        return self.row[4]
+    
+    def __str__(self):
+        return "Omoc: %s [%s] - %s"%(self.fromDate(),self.fromRoundDate(),self.toDate())
+    
+    def asDisplayString(self):
+        return "%s > %s"%(self.fromDate(),self.toDate())
+
+class LkrAdapter():
+    #03.07.2023-16:30-18:00-Berufsschule Weilheim Sporthalle-3-15
+    FIELDS = (9,10,12,13,14,15) #A=0
+
+    def __init(self,tupleRow):
+        self.row=tupleRow()
+        
+    def fromDate(self):
+        raw=self._cleanDate() #has space in front..
+        hm = self.row[1]
+        return datetime.strptime(hm+raw,"%H:%M %d.%m.%Y")
+    
+    def fromRoundDate(self):
+        raw=self.fromDate()
+        #lkr sometimes has 35. Round that to 30
+        return raw - timedelta(minutes=raw.minute %10)
+        
+    def toDate(self):    
+        raw=self._cleanDate()
+        hm = self.row[2]
+        return datetime.strptime(hm+raw,"%H:%M %d.%m.%Y")        
+    
+    def toRoundDate(self):
+        raw=self.toDate()
+        #lkr sometimes has 35. Round that to 30
+        return raw - timedelta(minutes=raw.minute %10)    
+    
+    def timeDelta(self):
+        return self.toRoundDate()-self.fromRoundDate()
+    
+    def _cleanDate(self):
+        return " "+self.row[0]
+    
+    def location(self):
+        return self.row[3]
+    
+    def timeUnits(self):
+        return self.row[4]
+    
+    def price(self):
+        return self.row[5]
+    
+    def isBlocked(self):
+        return False #no rules
+    
+    def __str__(self):
+        return "LKR: %s [%s] - %s "%(self.fromDate(),self.fromRoundDate(),self.toDate())
+    
+    def asDisplayString(self):
+        return "%s > %s"%(self.fromDate(),self.toDate())
+    
+
+class BookingEntry():
+    def __init__(self):
+        self.date=None
+        self.lkr=None
+        self.omc=None
+    
+    def omcDisplay(self):
+        if self.omc:
+            return self.omc.asDisplayString()
+        return "n.V"
+    
+    def lkrDisplay(self):
+        if self.lkr:
+            return self.lkr.asDisplayString()
+        return "n.V"
+
+    
+class WorkSheetWriter():
     def __init__(self,filePath):
         self.file=filePath
     
-    def exportXls(self,crazyArray):
-        pass
+    def export(self,title,header,crazyArray):
+        #fontStyle = Font(size = "10")
+        #ws['B3'].font = Font(bold=True)
+        wb = Workbook()
+        ws = wb.active
+        ws.title=title
+        ws.append([title])
+        ws["A1"].font = Font(size = "14",bold=True)
+        ws.append(header)        
+        for idx in range(0,len(header)):
+            char=chr(65+idx)
+            ws[char+str(2)].font=Font(bold=True)
+
+        for row in crazyArray:
+            ws.append(row)
+            
+        self._setColumnDimensions(ws)
+
+        wb.save(filename=self.file)
+        
+    def _setColumnDimensions(self,ws):
+        dims={}
+        for row in ws.rows:
+            for cell in row:
+                dims[cell.column_letter]=max((dims.get(cell.column_letter, 0), len(str(cell.value)))) 
+
+        for col, value in dims.items():
+            ws.column_dimensions[col].width = value                
     
 class WorkSheetComparer():
-    def __init__(self,lwArrray,omocArray):
-        pass       
+    def __init__(self):
+        self.omocSheet=None
+        self.lkrSheet=None
+        self.data=[] #Date, omoc,LKR
+        self.statistics=() #count same,count different == Statistics 
+        self.location = None #Kontext for the Writen, later. which location
+        
+    def readOmoc(self,omocPath):
+        wr=WorksheetReader(omocPath)
+        self.omocSheet = wr.importXls(OmocAdapter)
+        self.omocSheet.sort(key=lambda adapter: adapter.fromDate() )
+    
+    def readLkr(self,lkrPath):
+        wr=WorksheetReader(lkrPath)
+        self.lkrSheet = wr.importXls(LkrAdapter)
+        self.lkrSheet.sort(key=lambda adapter: adapter.fromDate() )        
+
+    def compare(self):
+        dic={} #Date ->bookingEntry
+        for adapter in self.omocSheet:
+            be = dic.get(adapter.fromRoundDate(),BookingEntry())
+            be.omc=adapter
+            dic[adapter.fromRoundDate()]=be
+            
+        for adapter in self.lkrSheet:
+            be = dic.get(adapter.fromRoundDate(),BookingEntry())
+            be.lkr=adapter
+            dic[adapter.fromRoundDate()]=be
+
+        same=0
+        diff=0
+        for key,bookingEntry in dic.items():
+            if bookingEntry.omc is not None and bookingEntry.lkr is not None and bookingEntry.omc.timeDelta()==bookingEntry.lkr.timeDelta():
+                same += 1
+            else:
+                #print(key,"O:",bookingEntry.omc," L:",bookingEntry.lkr)
+                self.data.append([str(key),bookingEntry.omcDisplay(),bookingEntry.lkrDisplay()])
+                diff += 1
+     
+        self.statistics=(same,diff)
+        #print("Same: %d diff: %d"%(same,diff))
+                
+           
+def test1():
+    path = OSTools.getLocalPath(__file__)
+    #p1 = OSTools.joinPathes(path, "testData", "OMOC Daten","20240116_FOS_OMOC.xlsx")
+    #p2 = OSTools.joinPathes(path, "testData", "Landkreislisten","FOS-Landkreis.xlsx")
+    #p1 = OSTools.joinPathes(path, "testData", "OMOC Daten","20240116_Jahnhalle_OMOC.xlsx")
+    #p2 = OSTools.joinPathes(path, "testData", "Landkreislisten","Jahnhalle-Landkreis.xlsx")
+    #p1 = OSTools.joinPathes(path, "testData", "OMOC Daten","20240116_Gymnasium_OMOC.xlsx")
+    #p2 = OSTools.joinPathes(path, "testData", "Landkreislisten","GYMLandkreis.xlsx")
+    p1 = OSTools.joinPathes(path, "testData", "OMOC Daten","20240116_Hallenbad_OMOC.xlsx")
+    p2 = OSTools.joinPathes(path, "testData", "Landkreislisten","Schwimmbad-Landkreis.xlsx")
+    
+    wc = WorkSheetComparer()
+    wc.readOmoc(p1)
+    wc.readLkr(p2)
+    wc.compare()
+    #testest
+    target=OSTools.joinPathes(path,"testData","Frigin.xlsx")
+    wr=WorkSheetWriter(target)
+    wr.export("Hallenbad",("Datum","OMOC","LKR"),wc.data)
+ 
+
 
 if __name__ == '__main__':
-    pass
+    test1()
--- src/OmocComparer.py
+++ src/OmocComparer.py
@@ -3,12 +3,16 @@ Created on 18 Jan 2024
 QT6 implementation of Xcel sheet comparison by a retro "Landkreis" and a fairly modern web app
 @author: matze
 '''
-import traceback,os,argparse,sys,json
+import traceback, os, argparse, sys, json
 from PyQt6.QtWidgets import QApplication, QTextEdit
-from PyQt6.QtCore import Qt,pyqtSignal
+from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6 import QtWidgets, QtGui, QtCore
 import OCModel
-from OCModel import Log,OSTools
+from OCModel import Log, OSTools, WorkSheetComparer, WorkSheetWriter
+from time import sleep
+
+VERSION="0.9"
+WIN=None
 
 '''
 Unbelievable windows crap: To get your icon into the task bar:
@@ -19,22 +23,29 @@ if os.name == 'nt':
     cwdll = ctypes.windll  # @UndefinedVariable
     cwdll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
+
 # #Main App Window. 
 class MainFrame(QtWidgets.QMainWindow):
-    
+    OMOC = "OMOC"
+    LKR = "LKR"
+    HALLEN=("Jahnhalle","Gymnasium","FOS","Schwimmbad","Sonstige")
+
     def __init__(self, qapp):
         super(MainFrame, self).__init__()
         self._isStarted = False
         self.__qapp = qapp
         self.qtQueueRunning = False
-        self.sourceFile=None
-        self.model=None
+        self.omocFile = None
+        self.lkrFile = None
+        self.configJson=None
+        self.model = WorkSheetComparer()
+        self.tmpSaveTarget=""
         self.setWindowIcon(getAppIcon())
         self.initUI()
         self.centerWindow()
-        self.setWindowTitle("TSV Omoc Whatever")
+        self.setWindowTitle("TSV Omoc Hallenputzer")
         self.show()
-        qapp.applicationStateChanged.connect(self.__queueStarted)        
+        # qapp.applicationStateChanged.connect(self.__queueStarted)        
 
     def centerWindow(self):
         frameGm = self.frameGeometry()
@@ -42,17 +53,20 @@ class MainFrame(QtWidgets.QMainWindow):
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
 
+    '''
     def __queueStarted(self, _state):
         if self.qtQueueRunning:
             return
         self.qtQueueRunning = True
         #self._initModel()
-        
+    '''
+    '''    
     def initUI(self):
+        self.init_toolbar()
         self.uiInfoLabel=QTextEdit(self)
         self.uiInfoLabel.setReadOnly(True)
         self.uiInfoLabel.setAcceptRichText(True)
-        self.uiInfoLabel.setText("<h3>Omoc converter<h3> ..setting up")
+        self.uiInfoLabel.setText("<h3>Omoc converter<\h3> <h4>Omoc Datei laden <img src=./icons/DownloadGreen.png width='26'> <br>Dazugehörige LKR Datei laden <img src=./icons/DownloadRed.png width='26'><\br><\h4>")
         self.uiInfoLabel.setAlignment(Qt.AlignmentFlag.AlignTop)
         box = QtWidgets.QVBoxLayout();
         box.addWidget(self.uiInfoLabel)        
@@ -62,9 +76,280 @@ class MainFrame(QtWidgets.QMainWindow):
         # TODO: self.resize
         wid.setLayout(box)
         self.resize(400, 450)
+        
+    def init_toolbar(self):
+        self.omocAction = QtGui.QAction(QtGui.QIcon('./icons/DownloadGreen.png'), 'Omoc Datei', self)
+        self.omocAction.setShortcut('Ctrl+O')
+        self.omocAction.triggered.connect(self._onOmocFileClicked)
+
+        self.lkrAction = QtGui.QAction(QtGui.QIcon('./icons/DownloadRed.png'), 'LKR Datei', self)
+        self.lkrAction.setShortcut('Ctrl+L')
+        self.lkrAction.triggered.connect(self._onLkrFileClicked)
+
+
+        self.stopAction = QtGui.QAction(QtGui.QIcon('./icons/dialog-close.png'), 'Schließen', self)
+        self.stopAction.setShortcut('Ctrl+H')
+        self.stopAction.triggered.connect(self.goodbye)
+        #self.stopAction.setEnabled(False)
+        
+        spacer = QtWidgets.QWidget();
+        spacer.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Preferred);
+        
+        self.toolbar = self.addToolBar('Main')
+        self.toolbar.addAction(self.omocAction)
+        self.toolbar.addAction(self.lkrAction)
+        self.toolbar.addSeparator()
+        self.toolbar.addWidget(spacer);
+        self.toolbar.addAction(self.stopAction)   
+    '''     
+
+    def initUI(self):
+        
+        pix = QtGui.QPixmap("./icons/TSV-big.png")
+        pix = pix.scaledToWidth(32, mode=Qt.TransformationMode.SmoothTransformation)
+        self.uiLogo = QtWidgets.QLabel("")
+        self.uiLogo.setPixmap(pix)
+        
+        self.uiTypeCombo = QtWidgets.QComboBox(self)
+        self.uiTypeCombo.setEditable(False)
+        #self.uiTypeCombo.currentTextChanged.connect(self._onTypeChanged)
+        self.uiTypeCombo.setToolTip("Welche Halle wird verglichen?")
+        self.uiTypeCombo.addItem("-Bitte Halle auswählen-")
+        self.uiTypeCombo.addItems(self.HALLEN) 
+        
+        self.uiOmocFile= QtWidgets.QLabel(self)
+        self.uiOmocFile.setText("?")
+        self.uiOmocButton = QtWidgets.QPushButton(" Omoc")
+        self.uiOmocButton.setIcon(QtGui.QIcon("./icons/download-folder.png"))
+        self.uiOmocButton.setFixedWidth(85)
+        self.uiOmocButton.setToolTip("Suche passende OMOC Datei")
+        self.uiOmocButton.clicked.connect(self._onOmocFileClicked)
+
+        self.uiLkrFile= QtWidgets.QLabel(self)
+        self.uiLkrFile.setText("?")
+        self.uiLkrButton = QtWidgets.QPushButton(" Lkr    ")
+        self.uiLkrButton.setIcon(QtGui.QIcon("./icons/download-folder.png"))
+        self.uiLkrButton.setFixedWidth(85)
+        self.uiLkrButton.setToolTip("Suche passende Landkreis Datei")
+        self.uiLkrButton.clicked.connect(self._onLkrFileClicked)
+
+        self.uiInfoLabel = QTextEdit(self)
+        self.uiInfoLabel.setReadOnly(True)
+        self.uiInfoLabel.setAcceptRichText(True)
+        self.uiInfoLabel.setHtml(self._intro())
+        self.uiInfoLabel.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        self.uiCompareButton = QtWidgets.QPushButton("Vergleiche")
+        self.uiCompareButton.setIcon(QtGui.QIcon("./icons/merge.png"))
+        self.uiCompareButton.clicked.connect(self._onCompareClicked)
+        self.uiCompareButton.setEnabled(False)
+        
+        self.uiExitButton = QtWidgets.QPushButton("Exit")
+        self.uiExitButton.setIcon(QtGui.QIcon("./icons/dialog-close.png"))
+        self.uiExitButton.clicked.connect(self._onExitClicked)
+
+        self.uiTypeCombo.currentTextChanged.connect(self._onTypeChanged) #make sure button exists
+
+        box = self._makeLayout()
+        wid = QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)    
+        wid.setLayout(box)
+        self.resize(500, 600) 
+      
+    def _makeLayout(self):
+        mainBox = QtWidgets.QVBoxLayout()  # for all
+        
+        headBox = QtWidgets.QHBoxLayout()
+        headBox.setSpacing(100)
+        headBox.addWidget(self.uiTypeCombo,stretch=50)
+        headBox.addWidget(self.uiLogo,stretch=0)
+        #headBox.addWidget(self.uiLogo)
+        #headBox.addStretch()
+        
+        btn1Box = QtWidgets.QHBoxLayout()  # btn omoc
+        btn1Box.setSpacing(20)
+        btn1Box.addWidget(self.uiOmocFile)
+        btn1Box.addStretch()
+        btn1Box.addWidget(self.uiOmocButton)
+        
+        btn2Box = QtWidgets.QHBoxLayout()  # btn lkr
+        btn2Box.setSpacing(20)
+        btn2Box.addWidget(self.uiLkrFile)
+        btn2Box.addStretch()
+        btn2Box.addWidget(self.uiLkrButton)
+        
+        btn3Box = QtWidgets.QHBoxLayout()  # btn bottom
+        btn3Box.setSpacing(20)
+        btn3Box.addStretch()
+        btn3Box.addWidget(self.uiCompareButton)
+        btn3Box.addWidget(self.uiExitButton)
+        
+        #mainBox.addWidget(self.uiTypeCombo)
+        mainBox.addLayout(headBox)
+        mainBox.addLayout(btn1Box)
+        mainBox.addLayout(btn2Box)
+        mainBox.addWidget(self.uiInfoLabel)
+        mainBox.addLayout(btn3Box)
+        
+        return mainBox
+
+    @QtCore.pyqtSlot()        
+    def _onOmocFileClicked(self):
+        targetPath = self.readLastXPath(self.OMOC)
+        extn = "*.xlsx"
+        fileFilter = "Spreadsheets (%s);;Alle Dateien(*.*)" % extn
+        result = QtWidgets.QFileDialog.getOpenFileName(parent=self, directory=targetPath, caption="Lese Omoc Daten", filter=fileFilter);
+        if result[0]:
+            self.omocFile = result[0]
+            self.uiOmocFile.setText(OSTools.fileNameOnly(self.omocFile))
+            self.saveLastXPath(self.OMOC,OSTools.dirname(self.omocFile))
+            self.uiInfoLabel.append("OMOC Datei gefunden")
+            self._updateCompareButton()
+
+    def _onLkrFileClicked(self):
+        targetPath = self.readLastXPath(self.LKR)
+        extn = "*.xlsx"
+        fileFilter = "Spreadsheets (%s);;Alle Dateien(*.*)" % extn
+        result = QtWidgets.QFileDialog.getOpenFileName(parent=self, directory=targetPath, caption="Lese LKR Daten", filter=fileFilter);
+        if result[0]:
+            self.lkrFile = result[0]
+            self.uiLkrFile.setText(OSTools.fileNameOnly(self.lkrFile))
+            self.saveLastXPath(self.LKR,OSTools.dirname(self.lkrFile))
+            self.uiInfoLabel.append("Landkreis Datei gefunden")
+            self._updateCompareButton()
+
+    @QtCore.pyqtSlot()
+    def _onExitClicked(self):
+        self.close()
+        
+    def _onTypeChanged(self, aString):
+        self.model.location = aString
+        self._updateCompareButton()
+
+    def _onCompareClicked(self):
+        #we need a long running operation
+        self.uiInfoLabel.append("Starte Vergleich ....")
+        QApplication.setOverrideCursor(QtCore.Qt.CursorShape.WaitCursor)
+        worker = LongRunningOperation(self._startCompare)
+        worker.signal.connect(self._cleanupWorker)
+        worker.startOperation()
+        
+    def _startCompare(self):
+        self.model.readOmoc(self.omocFile)
+        self.model.readLkr(self.lkrFile)
+        self.model.compare()
+        hp = OSTools.getHomeDirectory()
+        #tp = OSTools.joinPathes(path,"OMOC")
+        loc = self.model.location
+        self.tmpSaveTarget=OSTools.joinPathes(hp,loc+".xlsx")
+        wr=WorkSheetWriter(self.tmpSaveTarget)
+        wr.export(loc,("Datum","OMOC","LKR"),self.model.data)
+        
+        
+        
+    def _cleanupWorker(self, worker):
+        # QThread: Destroyed while thread is still running
+        msg = worker.msg 
+        Log.info("Long operation done:%s",msg)
+        if msg:
+            self.uiInfoLabel.setHtml('<p style="color:red;">Fehler:  %s</p><br>Nicht nochmal probieren -> Melden macht frei!'%(msg))
+        else:
+            self._reportResults()
+        worker.quit()
+        worker.wait(); 
+        QApplication.restoreOverrideCursor()   
+        self._prepareNextRun() 
+
+    def _reportResults(self):
+        txt=[]
+        txt.append("<h4> Vorgang beendet</h4>")
+        txt.append("Buchungen gleich: %d"%(self.model.statistics[0]))
+        txt.append("<br>Buchungen falsch: %d"%(self.model.statistics[1]))
+        txt.append("<br>Die Ergebnisdatei wurde unter %s abgelegt"%(self.tmpSaveTarget))
+        Log.info("Statistics Same:%d diff: %d",self.model.statistics[0],self.model.statistics[1])
+        self.uiInfoLabel.setHtml(''.join(txt))
+
+
+    def _intro(self):
+        txt=[]
+        txt.append("<h3>Omoc converter V%s</h3>"%(VERSION))
+        txt.append(" Bitte folgende Schritte durchführen ")
+        txt.append("<ul><li>Die prüfende Halle aussuchen</li>")
+        txt.append("<li>Die OMOC Datei wählen <strong>[Button]</strong></li>")
+        txt.append("<li>Die Landkreis(LKR) Datei wählen <strong>[Button]</strong></li>")
+        txt.append("<li>Vergleiche<strong>[Button]</strong></li>")
+        txt.append("</ul>")
+        txt.append("<br>Viel Erfolg")
+        return ''.join(txt)
+        
+
+    def _updateCompareButton(self):
+        if not self.uiCompareButton:
+            return
+        if not self.model.location.startswith("-"): 
+            if len(self.uiOmocFile.text()) > 3 and len(self.uiLkrFile.text()) > 3:
+                self.uiCompareButton.setEnabled(True)
+                return
+        self.uiCompareButton.setEnabled(False)
+
+    def _prepareNextRun(self):
+        #self.uiInfoLabel.setHtml(self)
+        self.uiTypeCombo.setCurrentIndex(0)
+        self.uiLkrFile.setText("?")
+        self.uiOmocFile.setText("?")
+        self.tmpSaveTarget=""
+        #self.uiCompareButton.setEnabled(False)
+        
+
+    def readLastXPath(self, part):
+        if not self.configJson:
+            cfg = self._configPath()
+            if OSTools.fileExists(cfg):
+                with open(cfg) as jsonFile:
+                    self.configJson = json.load(jsonFile)
+            
+        return self.configJson.get(part,None)
+
+    def saveLastXPath(self,part,path):
+        self.configJson[part]=path
+        cfg = self._configPath()
+        with open(cfg, 'w') as jsonFile:
+            json.dump(self.configJson, jsonFile)
+
+    def _configPath(self):
+        folder = OSTools.joinPathes(OSTools().getHomeDirectory(), ".config")
+        OSTools.ensureDirectory(folder)
+        return OSTools.joinPathes(folder, "omoc.json") 
+
+    
+''' Long running operations for actions that do not draw or paint '''        
+class LongRunningOperation(QtCore.QThread):
+    signal = pyqtSignal(object) 
+
+    def __init__(self, func, *args):
+        QtCore.QThread.__init__(self)
+        self.function = func
+        self.arguments = args
+        self.msg=None
+
+    def run(self):
+        try:
+            self.function(*self.arguments)
+        except Exception as ex:
+            Log.exception("***Error in LongRunningOperation***")
+            self.msg = str(ex)
+        finally:
+            self.signal.emit(self)
+
+    def startOperation(self):
+        self.start()  # invokes run - process pending QT events
+        sleep(0.5)
+        QtCore.QCoreApplication.processEvents() 
+        print("Events processed")   
+
 
 def getAppIcon():
-    return QtGui.QIcon('./icons/TSV-import.png')
+    return QtGui.QIcon('./icons/TSV-control.png')
 
 
 def handle_exception(exc_type, exc_value, exc_traceback):
@@ -72,15 +357,16 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     if WIN is not None:
         infoText = str(exc_value)
         detailText = "*".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-        WIN.getErrorDialog("Unexpected error", infoText, detailText).show()
+        #WIN.getErrorDialog("Unexpected error", infoText, detailText).show()
         Log.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 
 def parse():
     parser = argparse.ArgumentParser(description="OmocComparer")
     parser.add_argument('-d', dest="debug", action='store_true', help="Debug logs")
-    parser.add_argument('-f', dest="headless", type=str, default=None, help="execute without GUI with filename",metavar="FILE")
+    #parser.add_argument('-f', dest="headless", type=str, default=None, help="execute without GUI with filename", metavar="FILE")
     return parser.parse_args() 
+
 
 def main(args):
     try:
@@ -94,8 +380,8 @@ def main(args):
             OSTools.setLogLevel("Debug")
         else:
             OSTools.setLogLevel("Info")
-        #fn = args.headless
-        #if fn is not None:
+        # fn = args.headless
+        # if fn is not None:
         #    headlessImport(fn)
         #    return 0
         Log.info("--- Start Omoc comparer ---")
@@ -108,13 +394,16 @@ def main(args):
         app.exec()
         
     except:
-        with open('/tmp/error.log','a') as f:
+        with open('/tmp/error.log', 'a') as f:
             f.write(traceback.format_exc())
         traceback.print_exc(file=sys.stdout)
         Log.exception("Error in main:")
         # ex_type, ex_value, ex_traceback
         sys_tuple = sys.exc_info()
         QtWidgets.QMessageBox.critical(None, "Error!", str(sys_tuple[1]))
+
+
 if __name__ == '__main__':
     sys.excepthook = handle_exception
     sys.exit(main(parse()))
+    
